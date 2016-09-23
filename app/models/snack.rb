a6675b466da9c7e7544c9116a1da122a06a827ad
holsi006 <holsi006@umn.edu>
2016-09-22 21:17:18 -0500

    modified:   app/models/snack.rb

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,7 +1,9 @@
 include ActionView::Helpers::NumberHelper 
 
 class Snack < ActiveRecord::Base
+mount_uploader :image, ImageUploader
   def get_formatted_price
     number_to_currency(price)
   end
+
 end
