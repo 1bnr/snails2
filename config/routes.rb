5dd0f34d65284c356b78d548ca2ddab0fbb65022
1bnr <9zabner@gmail.com>
2017-11-07 19:13:23 -0600

    Hack "/snacks" into each route
    this should fix the misrouting problem

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
 
-
+namespace "snacks" do
+  # Put your resources here, as however you defined them before, e.g.:  
+  resources :deposits, :accounts, :users, :snacks, :orders, :line_itmes, :deposits
+end
+  
   devise_scope :user do
       get "users/show", to: "users/sessions#show"
       get "users/new", to: "users/sessions#new"
